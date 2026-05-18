f0f48444d205288476addeb5f1d99020b499253b
Sabelo Mkhabela <transformmkhabela@gmail.com>
2026-05-18 08:48:02 +0200

    fix: port number 3000 preventing deploy

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const { v4: uuidv4 } = require('uuid');
 const db = require('./db');
 
 const app  = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 
 // ── Middleware ────────────────────────────────────────────────
 app.use(express.urlencoded({ extended: true }));
